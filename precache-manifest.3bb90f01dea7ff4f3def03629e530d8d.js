3928e57a88e92db84d74bc306d06b74c1e05fa8d
Michael Busby <prodigysim@gmail.com>
2020-10-10 09:22:16 -0500

    Updates

---
+++ precache-manifest.3bb90f01dea7ff4f3def03629e530d8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8ce734c379a84039e83edf04ed5f22",
+    "url": "/l4d2-tools/index.html"
+  },
+  {
+    "revision": "ac71c2af810410af2c66",
+    "url": "/l4d2-tools/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44eb90a766c3e4233aea",
+    "url": "/l4d2-tools/static/js/2.5aad20e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/l4d2-tools/static/js/2.5aad20e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac71c2af810410af2c66",
+    "url": "/l4d2-tools/static/js/main.c693fd08.chunk.js"
+  },
+  {
+    "revision": "986abc0fed3cd5518468",
+    "url": "/l4d2-tools/static/js/runtime-main.5cab08ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/l4d2-tools/static/media/logo.5d5d9eef.svg"
+  }
+]);
